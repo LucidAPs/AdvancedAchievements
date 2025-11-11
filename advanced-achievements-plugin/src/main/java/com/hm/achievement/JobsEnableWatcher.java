da2abb6cb13b0e5818a9feb3bb2f70c7227edfe2
LucidAPs <lucidap@live.com>
2025-11-11 13:09:50 +0200

    Better jobs support

--- advanced-achievements-plugin/src/main/java/com/hm/achievement/JobsEnableWatcher.java
+++ advanced-achievements-plugin/src/main/java/com/hm/achievement/JobsEnableWatcher.java
@@ -3,10 +3,12 @@ package com.hm.achievement;
 import com.hm.achievement.exception.PluginLoadError;
 import com.hm.achievement.lifecycle.PluginLoader;
 import javax.inject.Inject;
+import javax.inject.Provider;
 import javax.inject.Singleton;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.server.PluginEnableEvent;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,20 +16,22 @@ import java.util.logging.Logger;
 @Singleton
 public class JobsEnableWatcher implements Listener {
     private final AdvancedAchievements aa;
-    private final PluginLoader loader;
+    private final Provider<PluginLoader> loaderProvider; // <-- Provider breaks the cycle
     private final Logger log;
 
     @Inject
-    public JobsEnableWatcher(AdvancedAchievements aa, PluginLoader loader, Logger log) {
-        this.aa = aa; this.loader = loader; this.log = log;
+    public JobsEnableWatcher(AdvancedAchievements aa, Provider<PluginLoader> loaderProvider, Logger log) {
+        this.aa = aa;
+        this.loaderProvider = loaderProvider;
+        this.log = log;
     }
 
     @EventHandler
-    public void onPluginEnable(org.bukkit.event.server.PluginEnableEvent e) {
+    public void onPluginEnable(PluginEnableEvent e) {
         if (e.getPlugin().getName().equalsIgnoreCase("Jobs")) {
             Bukkit.getScheduler().runTask(aa, () -> {
                 try {
-                    loader.loadAdvancedAchievements(); // mirrors /aach reload
+                    loaderProvider.get().loadAdvancedAchievements(); // mirrors /aach reload
                     log.info("[AdvancedAchievements] Jobs enabled; JobsReborn category now active.");
                 } catch (PluginLoadError ex) {
                     log.log(Level.SEVERE, "Could not enable JobsReborn category after Jobs enable:", ex);
@@ -36,4 +40,3 @@ public class JobsEnableWatcher implements Listener {
         }
     }
 }
-
